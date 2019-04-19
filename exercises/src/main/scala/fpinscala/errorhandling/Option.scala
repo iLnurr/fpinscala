aafccbcdd6b3881639f246573d95505a32b05971
serbaev_ilnur <serbaevilnur@gmail.com>
2019-04-19 11:07:53 +0500

    Ch4 option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -50,11 +50,27 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap { m ⇒
+    val res  = xs.map(x ⇒ math.pow(x - m, 2))
+    mean(res)
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a,b) match {
+    case (None,_) ⇒ None
+    case (_,None) ⇒ None
+    case (Some(`a`),Some(`b`)) ⇒ Some(f(`a`,`b`))
+  }
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(List.empty[A]))((opt, acc) ⇒ map2(opt,acc)(_ :: _))
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(List.empty[B]))((aa,acc) ⇒ map2(f(aa),acc)(_ :: _))
+
+  def sequence2[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(identity)
 }
