0974fe65cd835c3126c6751e8b0046f75cfaa9a4
serbaev_ilnur <serbaevilnur@gmail.com>
2019-06-27 11:05:12 +0500

    streaming io ex11

--- exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -829,10 +829,15 @@ object GeneralizedStreamTransducers {
         { src => eval_ { IO(src.close) } }
 
     /* Exercise 11: Implement `eval`, `eval_`, and use these to implement `lines`. */
-    def eval[F[_],A](a: F[A]): Process[F,A] = ???
+    def eval[F[_],A](a: F[A]): Process[F,A] =
+      await(a) {
+        case Left(err) => Halt(err)
+        case Right(value) => Emit[F,A](value, Halt(End))
+      }
 
     /* Evaluate the action purely for its effects. */
-    def eval_[F[_],A,B](a: F[A]): Process[F,B] = ???
+    def eval_[F[_],A,B](a: F[A]): Process[F,B] =
+      eval_(a).drain[B]
 
     /* Helper function with better type inference. */
     def evalIO[A](a: IO[A]): Process[IO,A] =
